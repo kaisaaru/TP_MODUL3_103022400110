19f3d40172a59825f6d1b87f8bfb3a12157ec002
Kaisar Rayfa Al Baihaqqi <ryf.kaisaaru@gmail.com>
2026-03-08 14:33:46 +0700

    Menambahkan implementasi kalkulator

--- TP_MODUL3_103022400110/Form1.Designer.cs
+++ TP_MODUL3_103022400110/Form1.Designer.cs
@@ -51,6 +51,7 @@
             button1.TabIndex = 0;
             button1.Text = "1";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
@@ -70,6 +71,7 @@
             button3.TabIndex = 2;
             button3.Text = "3";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // button4
             // 
@@ -79,6 +81,7 @@
             button4.TabIndex = 3;
             button4.Text = "4";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // button5
             // 
@@ -88,6 +91,7 @@
             button5.TabIndex = 4;
             button5.Text = "5";
             button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
             // 
             // button6
             // 
@@ -97,6 +101,7 @@
             button6.TabIndex = 5;
             button6.Text = "6";
             button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
             // 
             // button7
             // 
@@ -106,6 +111,7 @@
             button7.TabIndex = 6;
             button7.Text = "7";
             button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
             // 
             // button8
             // 
@@ -115,6 +121,7 @@
             button8.TabIndex = 7;
             button8.Text = "8";
             button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
             // 
             // button9
             // 
@@ -124,6 +131,7 @@
             button9.TabIndex = 8;
             button9.Text = "9";
             button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
             // 
             // button10
             // 
@@ -133,6 +141,7 @@
             button10.TabIndex = 9;
             button10.Text = "+";
             button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
             // 
             // button11
             // 
@@ -142,6 +151,7 @@
             button11.TabIndex = 10;
             button11.Text = "0";
             button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
             // 
             // button12
             // 
@@ -151,11 +161,12 @@
             button12.TabIndex = 11;
             button12.Text = "=";
             button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(178, 39);
+            label1.Location = new Point(78, 41);
             label1.Name = "label1";
             label1.Size = new Size(95, 20);
             label1.TabIndex = 12;
